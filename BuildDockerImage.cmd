b8cf25b977887eb1de7318e063b4878839dbff40
thabart <habarthierry@hotmail.fr>
2016-07-01 17:59:44 +0200

    Fix the docker files

--- BuildDockerImage.cmd
+++ BuildDockerImage.cmd
@@ -4,11 +4,20 @@ docker build -t uma -f Dockerfile-Uma .
 docker build -t websiteapi -f Dockerfile-WebSiteApi .
 docker build -t website -f Dockerfile-WebSite .
 
+# delete all images
+docker rm -f $(docker ps -aq)
+docker rmi simpleidentityserverdocker_websiteapi
+docker rmi simpleidentityserverdocker_simpleidserver
+docker rmi simpleidentityserverdocker_configuration
+docker rmi simpleidentityserverdocker_website
+docker rmi simpleidentityserverdocker_uma
+docker rmi simpleidentityserverdocker_manager
+
 # Run postgresql
 docker run --name postgresql -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -d postgres
 
 # Run simpleidserver
-docker run --name simpleidserver -e DB_ALIAS=postgresql -e DB_PORT=5432 --link postgresql simpleidserver
+docker run --name simpleidserver -e DB_ALIAS=postgresql -e DB_PORT=5432 -p 5443:5443 -p 5000:5000 --link postgresql -it simpleidserver /bin/bash
 
 # Run postgresql - uma
 docker run --name postgresql_uma -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -d postgres
@@ -21,3 +30,6 @@ docker run --name mongo -d mongo
 
 # Run application
 docker run --name websiteapi --link mongo:mongo -d websiteapi
+
+# Export certificate
+openssl x509 -inform der -in LokitCa.cer -out LokitCa.pem
